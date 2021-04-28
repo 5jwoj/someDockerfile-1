b71e39548466ed92aed29f8baaab0d9f8f70484c
Aaron-lv <674560606@qq.com>
2021-04-28 17:33:11 +0800

    更新京东红包雨脚本

--- jd_scripts/red_rain_remote.sh
+++ jd_scripts/red_rain_remote.sh
@@ -29,20 +29,18 @@ if [ -f "/scripts/jd_red_rain.js" ]; then
         done
     fi
 
-    if [[ "$(date +%-H)" -ge "20" && "$(date +%-H)" -le "23" ]]; then
-        if [ "$(date +%-M)" == "30" ]; then
-            if [ "$live_redrain_control" == "0" ]; then
-                echo "配置不运行超级直播间红包雨，跳过执行..."
-            else
-                echo "配置运行超级直播间红包雨，开始执行..."
-                lzz_half_url="https://ghproxy.com/https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd-half-rain.json"
-                lzz_half_id="$(curl -s $lzz_half_url)"
-                arr=${lzz_half_id//;/ }
-                for item in $arr; do
-                    sed -i "s/id = '.*$/id = '$item';/g" /scripts/jd_live_redrain.js
-                    $CMD /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1
-                done
-            fi
+    if [ "$(date +%-M)" == "30" ]; then
+        if [ "$live_redrain_control" == "0" ]; then
+            echo "配置不运行超级直播间红包雨，跳过执行..."
+        else
+            echo "配置运行超级直播间红包雨，开始执行..."
+            lzz_half_url="https://ghproxy.com/https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd-half-rain.json"
+            lzz_half_id="$(curl -s $lzz_half_url)"
+            arr=${lzz_half_id//;/ }
+            for item in $arr; do
+                sed -i "s/id = '.*$/id = '$item';/g" /scripts/jd_live_redrain.js
+                $CMD /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1
+            done
         fi
     fi
 
